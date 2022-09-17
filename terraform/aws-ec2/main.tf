042dbf0dd664a8819e8b38576b4dec364cb7497d
Tung Bui <tung.bquang@gmail.com>
2022-09-17 17:53:10 +0700

    Add AWS EC2 terraform

---
+++ terraform/aws-ec2/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
