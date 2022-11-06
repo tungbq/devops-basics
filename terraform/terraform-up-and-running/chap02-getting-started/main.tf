8d484161bf82c5dc8a25ed7c0116993e4f36675d
Tung Bui <tung.bquang@gmail.com>
2022-11-06 22:25:27 +0700

    [terraform] Deploy HTTP server

--- terraform/terraform-up-and-running/chap02-getting-started/main.tf
+++ terraform/terraform-up-and-running/chap02-getting-started/main.tf
@@ -2,9 +2,34 @@ provider "aws" {
   region = "us-east-2"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8080
+}
+
+# Create security group
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Launch an EC2 instance
 resource "aws_instance" "example" {
   ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   tags = {
     Name = "terraform-example-leo"
   }
