4ab4e2be7d41db956c7671df241fcb893daa2822
Tung Bui <tung.bquang@gmail.com>
2022-11-07 19:11:45 +0700

    fixes: outbound rule is missing]

--- terraform/terraform-up-and-running/chap02-getting-started/main.tf
+++ terraform/terraform-up-and-running/chap02-getting-started/main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type = number
-  default = 8080
+  default = 80
 }
 
 # Create security group
@@ -17,18 +17,28 @@ resource "aws_security_group" "instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 # Launch an EC2 instance
 resource "aws_instance" "example" {
-  ami = "ami-0c55b159cbfafe1f0"
+  ami = "ami-0c4e4b4eb2e11d1d4"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd.service
+              systemctl enable httpd.service
+              echo "[tungbq] This is $(hostname)" > /var/www/html/index.html
               EOF
   tags = {
     Name = "terraform-example-leo"
