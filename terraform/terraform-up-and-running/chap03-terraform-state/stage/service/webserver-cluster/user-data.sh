d3fd7e9442426d60e7ed9adbe57b5cf7842f2500
Tung Bui <tung.bquang@gmail.com>
2022-11-29 23:39:33 +0700

    Done

--- terraform/terraform-up-and-running/chap03-terraform-state/stage/service/webserver-cluster/user-data.sh
+++ terraform/terraform-up-and-running/chap03-terraform-state/stage/service/webserver-cluster/user-data.sh
@@ -7,7 +7,7 @@ systemctl start httpd.service
 touch /var/www/html/index.html
 
 cat > /var/www/html/index.html <<EOF
-<h1>Hello, World</h1>
+<h1>Hello, World! My name is Tung</h1>
 <p>DB address: ${db_address}</p>
 <p>DB port: ${db_port}</p>
 EOF
