2b4919affcc44f7b8cf1cd4cd98b7cf23b633867
Tung Bui <tung.bquang@gmail.com>
2022-12-21 22:24:06 +0700

    Implement extra input parameters

--- terraform/terraform-up-and-running/chap04-reusable-module/modules/services/asg-webserver-cluster/variables.tf
+++ terraform/terraform-up-and-running/chap04-reusable-module/modules/services/asg-webserver-cluster/variables.tf
@@ -7,3 +7,21 @@ variable "environment" {
   description = "ALB name to be created"
   type = string
 }
+
+variable "asg_min_size" {
+  description = "ASG min size"
+  type = number
+  default = 1
+}
+
+variable "asg_max_size" {
+  description = "ASG max size"
+  type = number
+  default = 2
+}
+
+variable "asg_desired_capacity" {
+  description = "ASG desired capacity"
+  type = number
+  default = 1
+}
