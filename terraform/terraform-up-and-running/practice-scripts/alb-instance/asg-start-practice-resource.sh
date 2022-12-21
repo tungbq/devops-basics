d2410f0e3ecba04eda3e8e1c76df2d43845b2b26
Tung Bui <tung.bquang@gmail.com>
2022-12-21 22:47:37 +0700

    Re-org practice scripts

--- terraform/terraform-up-and-running/practice-scripts/alb-instance/asg-start-practice-resource.sh
+++ terraform/terraform-up-and-running/practice-scripts/alb-instance/asg-start-practice-resource.sh
@@ -1,6 +1,6 @@
 # Define vars
 CURRENT_PATH=$(pwd)
-ROOT_PATH="$CURRENT_PATH/../../.."
+ROOT_PATH="$CURRENT_PATH/../../"
 CHAP03_PATH="$ROOT_PATH/chap03-terraform-state"
 CHAP04_PATH="$ROOT_PATH/chap04-reusable-module"
 
