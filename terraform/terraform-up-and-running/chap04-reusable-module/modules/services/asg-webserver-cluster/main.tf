00227f57b1aadb98896aeb8a4531a886669a8667
Tung Bui <tung.bquang@gmail.com>
2022-12-21 21:21:10 +0700

    Init chap04 asg from terraform src

--- terraform/terraform-up-and-running/chap04-reusable-module/modules/services/asg-webserver-cluster/main.tf
+++ terraform/terraform-up-and-running/chap04-reusable-module/modules/services/asg-webserver-cluster/main.tf
@@ -8,3 +8,116 @@ terraform {
     }
   }
 }
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.77.0"
+
+  name = "main-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs                  = data.aws_availability_zones.available.names
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+# Launch configuration
+resource "aws_launch_configuration" "terramino" {
+  name_prefix     = "learn-terraform-aws-asg-"
+  image_id        = data.aws_ami.amazon-linux.id
+  instance_type   = "t2.micro"
+  user_data       = file("user-data.sh")
+  security_groups = [aws_security_group.terramino_instance.id]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# ASG
+resource "aws_autoscaling_group" "terramino" {
+  min_size             = 1
+  max_size             = 1
+  desired_capacity     = 1
+  launch_configuration = aws_launch_configuration.terramino.name
+  vpc_zone_identifier  = module.vpc.public_subnets
+}
+
+# ALB
+resource "aws_lb" "terramino" {
+  name               = "learn-asg-terramino-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.terramino_lb.id]
+  subnets            = module.vpc.public_subnets
+}
+
+# ALB listener
+resource "aws_lb_listener" "terramino" {
+  load_balancer_arn = aws_lb.terramino.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.terramino.arn
+  }
+}
+
+# ALB target group
+ resource "aws_lb_target_group" "terramino" {
+   name     = "learn-asg-terramino"
+   port     = 80
+   protocol = "HTTP"
+   vpc_id   = module.vpc.vpc_id
+ }
+
+resource "aws_autoscaling_attachment" "terramino" {
+  autoscaling_group_name = aws_autoscaling_group.terramino.id
+  alb_target_group_arn   = aws_lb_target_group.terramino.arn
+}
+
+# SG-01: EC2 instance
+resource "aws_security_group" "terramino_instance" {
+  name = "learn-asg-terramino-instance"
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.terramino_lb.id]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.terramino_lb.id]
+  }
+
+  vpc_id = module.vpc.vpc_id
+}
+
+# SG-02: Load balancer
+resource "aws_security_group" "terramino_lb" {
+  name = "learn-asg-terramino-lb"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = module.vpc.vpc_id
+}
