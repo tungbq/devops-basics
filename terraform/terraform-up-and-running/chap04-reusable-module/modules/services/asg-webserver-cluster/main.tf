41bfd589ac33e5684b32a2cdc2f1f2248c0e0b70
Tung Bui <tung.bquang@gmail.com>
2022-12-21 21:23:01 +0700

    Update user data for ASG

--- terraform/terraform-up-and-running/chap04-reusable-module/modules/services/asg-webserver-cluster/main.tf
+++ terraform/terraform-up-and-running/chap04-reusable-module/modules/services/asg-webserver-cluster/main.tf
@@ -31,7 +31,7 @@ resource "aws_launch_configuration" "terramino" {
   name_prefix     = "learn-terraform-aws-asg-"
   image_id        = data.aws_ami.amazon-linux.id
   instance_type   = "t2.micro"
-  user_data       = file("user-data.sh")
+  user_data       = file("asg-user-data.sh")
   security_groups = [aws_security_group.terramino_instance.id]
 
   lifecycle {
