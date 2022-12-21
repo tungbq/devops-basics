76fe63f8bbefb8bff7e43e34eda61a3b615930c8
Tung Bui <tung.bquang@gmail.com>
2022-12-21 21:35:41 +0700

    Parameterize ALB name.1

--- terraform/terraform-up-and-running/chap04-reusable-module/stage/services/asg-webserver-alb/main.tf
+++ terraform/terraform-up-and-running/chap04-reusable-module/stage/services/asg-webserver-alb/main.tf
@@ -4,7 +4,5 @@ provider "aws" {
 module "webserver_alb" {
   source = "../../../modules/services/asg-webserver-cluster"
 
-  cluster_name = "webservers-stage"
-  db_remote_state_bucket = "tungleo-terraform-state-s3"
-  db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
+  alb_name = "testing-alb"
 }
