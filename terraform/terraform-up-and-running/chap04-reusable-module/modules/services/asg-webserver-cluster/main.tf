9bef4bd2ef074335e847dba0266b6409ecbe9744
Tung Bui <tung.bquang@gmail.com>
2022-12-21 21:58:17 +0700

    Fix missing ami linux

--- terraform/terraform-up-and-running/chap04-reusable-module/modules/services/asg-webserver-cluster/main.tf
+++ terraform/terraform-up-and-running/chap04-reusable-module/modules/services/asg-webserver-cluster/main.tf
@@ -13,6 +13,16 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+data "aws_ami" "amazon-linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*-x86_64-ebs"]
+  }
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.77.0"
