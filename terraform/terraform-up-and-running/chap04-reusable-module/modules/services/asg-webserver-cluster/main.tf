8a8068b66bca4d869f817308f7fe1861ca34b2a0
Tung Bui Quang (Leo) <tung.bquang@gmail.com>
2022-12-23 15:55:19 +0700

    More outputs + adding debugging inbound rule

--- terraform/terraform-up-and-running/chap04-reusable-module/modules/services/asg-webserver-cluster/main.tf
+++ terraform/terraform-up-and-running/chap04-reusable-module/modules/services/asg-webserver-cluster/main.tf
@@ -104,6 +104,20 @@ resource "aws_security_group" "terramino_instance" {
     security_groups = [aws_security_group.terramino_lb.id]
   }
 
+  # DEBUG-HTTP Inbound rules
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # DEBUG-SSH Inbound rules
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port       = 0
     to_port         = 0
--- terraform/terraform-up-and-running/chap04-reusable-module/modules/services/asg-webserver-cluster/outputs.tf
+++ terraform/terraform-up-and-running/chap04-reusable-module/modules/services/asg-webserver-cluster/outputs.tf
@@ -2,3 +2,15 @@ output "alb_dns_name" {
   value = aws_lb.terramino.dns_name
   description = "The domain name of the load balancer"
 }
+
+output "lb_endpoint" {
+  value = "https://${aws_lb.terramino.dns_name}"
+}
+
+output "application_endpoint" {
+  value = "https://${aws_lb.terramino.dns_name}/index.php"
+}
+
+output "asg_name" {
+  value = aws_autoscaling_group.terramino.name
+}
