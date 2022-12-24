303626f45495e3ef9fc04a74edf9f2869f2e077f
Tung Bui Quang (Leo) <tung.bquang@gmail.com>
2022-12-24 13:40:12 +0700

    tf: adding networking and SGs

--- terraform/aws-three-tier/modules/three-tier-deployment/networking/main.tf
+++ terraform/aws-three-tier/modules/three-tier-deployment/networking/main.tf
@@ -126,3 +126,134 @@ resource "aws_subnet" "three_tier_private_subnets_db" {
     Name = "three_tier_private_db${count.index + 1}"
   }
 }
+
+
+### SECURITY GROUPS
+
+resource "aws_security_group" "three_tier_bastion_sg" {
+  name        = "three_tier_bastion_sg"
+  description = "Allow SSH Inbound Traffic From Set IP"
+  vpc_id      = aws_vpc.three_tier_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.access_ip]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_security_group" "three_tier_lb_sg" {
+  name        = "three_tier_lb_sg"
+  description = "Allow Inbound HTTP Traffic"
+  vpc_id      = aws_vpc.three_tier_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "three_tier_frontend_app_sg" {
+  name        = "three_tier_frontend_app_sg"
+  description = "Allow SSH inbound traffic from Bastion, and HTTP inbound traffic from loadbalancer"
+  vpc_id      = aws_vpc.three_tier_vpc.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.three_tier_bastion_sg.id]
+  }
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.three_tier_lb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "three_tier_backend_app_sg" {
+  name        = "three_tier_backend_app_sg"
+  vpc_id      = aws_vpc.three_tier_vpc.id
+  description = "Allow Inbound HTTP from FRONTEND APP, and SSH inbound traffic from Bastion"
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.three_tier_frontend_app_sg.id]
+  }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.three_tier_bastion_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "three_tier_rds_sg" {
+  name        = "three-tier_rds_sg"
+  description = "Allow MySQL Port Inbound Traffic from Backend App Security Group"
+  vpc_id      = aws_vpc.three_tier_vpc.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.three_tier_backend_app_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+### DATABASE SUBNET GROUP
+
+resource "aws_db_subnet_group" "three_tier_rds_subnetgroup" {
+  count      = var.db_subnet_group == true ? 1 : 0
+  name       = "three_tier_rds_subnetgroup"
+  subnet_ids = [aws_subnet.three_tier_private_subnets_db[0].id, aws_subnet.three_tier_private_subnets_db[1].id]
+
+  tags = {
+    Name = "three_tier_rds_sng"
+  }
+}
