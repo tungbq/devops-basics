9505c97c383f999903fb18d8f3878e7bc3e0b24b
Tung Bui Quang (Leo) <tung.bquang@gmail.com>
2022-12-24 13:58:42 +0700

    Adding DB-var

---
+++ terraform/aws-three-tier/modules/three-tier-deployment/database/variables.tf
@@ -0,0 +1,12 @@
+# --- database/variables.tf ---
+
+variable "db_storage" {}
+variable "db_instance_class" {}
+variable "db_name" {}
+variable "dbuser" {}
+variable "dbpassword" {}
+variable "db_subnet_group_name" {}
+variable "db_engine_version" {}
+variable "db_identifier" {}
+variable "skip_db_snapshot" {}
+variable "rds_sg" {}
