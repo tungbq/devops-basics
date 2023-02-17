50681b923acf9f286c3de5b7791cf359bd1c4317
Tung Bui <tung.bquang@gmail.com>
2023-02-17 22:27:08 +0700

    ansible: update practice cmd

--- ansible/AnsibleEnv.dockerfile
+++ ansible/AnsibleEnv.dockerfile
@@ -4,5 +4,4 @@ RUN apt-get update && \
     apt-get install -y software-properties-common && \
     apt-add-repository --yes --update ppa:ansible/ansible && \
     apt-get install -y ansible
-
 CMD ["/bin/bash"]
--- ansible/ansible-practice.sh
+++ ansible/ansible-practice.sh
@@ -1,6 +1,6 @@
 echo ">>> Building ansible runner..."
 docker build -t ansible-runner -f AnsibleEnv.dockerfile .
 echo ">>> Execute ansible playbook (ping google)..."
-docker run --rm ansible-runner ansible-playbook ping-google.yaml
+docker run --rm -v "$(pwd)/basic":/basic -w /basic ansible-runner ansible-playbook ping-google.yaml
 echo ">>> Execute ansible playbook (check stats)..."
-docker run --rm ansible-runner ansible-playbook check-stats.yaml
+docker run --rm -v "$(pwd)/basic":/basic -w /basic ansible-runner ansible-playbook check-stats.yaml
