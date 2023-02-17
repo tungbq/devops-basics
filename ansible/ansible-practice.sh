d93bd42fda82b3cee446d214bbad73ce9eb4adec
Tung Bui Quang (Leo) <tung.bquang@gmail.com>
2023-02-17 21:51:53 +0700

    check stats

--- ansible/ansible-practice.sh
+++ ansible/ansible-practice.sh
@@ -1,4 +1,6 @@
 echo ">>> Building ansible runner..."
 docker build -t ansible-runner -f AnsibleEnv.dockerfile .
 echo ">>> Execute ansible playbook (ping google)..."
-docker run --rm ansible-runner ansible-playbook ping-google.yml
+docker run --rm ansible-runner ansible-playbook ping-google.yaml
+echo ">>> Execute ansible playbook (check stats)..."
+docker run --rm ansible-runner ansible-playbook check-stats.yaml
