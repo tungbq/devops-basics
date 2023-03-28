2acdc9994c4af4549706bdb8bd67e816bdfebb8e
Tung Bui <tung.bquang@gmail.com>
2023-03-28 21:25:14 +0700

    Adding basic example for shell

--- shell/basic.sh
+++ shell/basic.sh
@@ -1,3 +1,26 @@
-
 # Print Hello World
 echo "Hello, my name is Tung"
+# Get current date time
+date
+# Grep command
+echo "This is my testing string" | grep "my testing"
+
+# Sed command -> TODO
+
+# If - Else
+ADULT_AGE=18
+MY_AGE=28
+
+if [ $MY_AGE -gt $ADULT_AGE ]; then
+  echo "You are adult!"
+else
+  echo "You are under $ADULT_AGE!"
+fi
+
+# For - Loop
+
+## Switch - Case
+
+## json parsing
+
+## Regex
