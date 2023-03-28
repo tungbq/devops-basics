d0c136ea2a53cb503f0fc46b953f473dac79db4f
Tung Bui <tung.bquang@gmail.com>
2023-03-28 21:28:11 +0700

    content seperator

--- shell/basic.sh
+++ shell/basic.sh
@@ -1,17 +1,16 @@
 # Print Hello World
 echo "Hello, my name is Tung"
+
 # Get current date time
 TODAY_OUTPUT=$(date)
 echo "Today is: $TODAY_OUTPUT"
+
 # Grep command
 echo "This is my testing string" | grep "my testing"
-
-# Sed command -> TODO
 
 # If - Else
 ADULT_AGE=18
 MY_AGE=28
-
 if [ $MY_AGE -gt $ADULT_AGE ]; then
   echo "You are adult!"
 else
@@ -23,3 +22,4 @@ fi
 ## Switch - Case
 ## json parsing
 ## Regex
+## Sed command -> TODO
