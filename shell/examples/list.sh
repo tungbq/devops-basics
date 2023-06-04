8fdc326392479b1a5b1cfc6635b6155596f14121
Harshal Abak <abakharshal@gmail.com>
2023-06-04 22:19:17 +0530

    readme edit

--- shell/examples/list.sh
+++ shell/examples/list.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Define the output file name
+#In this script, we first define the output file name as file_list.txt. 
 output_file="file_list.txt"
 
-# Use the 'ls' command to list all .txt files in the current directory
+#Then, we use the ls command with the *.txt pattern to list all .txt files in the current directory. The > operator redirects the output of the ls command to the specified output file.
 ls *.txt > "$output_file"
 
-# Display a message indicating the completion of the task
+# Finally, we display a message indicating the completion of the task, specifying the name of the output file.
 echo "List of .txt files has been saved to $output_file"
+
+#After running the script, you will find the list of .txt files in the current directory stored in the file_list.txt file.
