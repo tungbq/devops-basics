a43f97624b164bafbbcb05d8d25e2329abc55e19
Tung Bui <tung.bquang@gmail.com>
2023-07-29 13:10:08 +0700

    helm: create jenkins-volume

--- helm/hands-on/deploy-jenkins/deploy.sh
+++ helm/hands-on/deploy-jenkins/deploy.sh
@@ -1,4 +1,6 @@
-# Init
+# Init and configure Helm
 echo "Adding Jenkins repo and get latest updates"
 helm repo add jenkinsci https://charts.jenkins.io
 helm repo update
+
+# Create Persistent Volume
