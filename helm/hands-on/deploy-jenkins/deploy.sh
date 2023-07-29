48c5c9e1b13898dc49475787a7fbfb840f430870
Tung Bui <tung.bquang@gmail.com>
2023-07-29 15:40:27 +0700

    helm/jenkins: better logging console

--- helm/hands-on/deploy-jenkins/deploy.sh
+++ helm/hands-on/deploy-jenkins/deploy.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+log() {
+  local msg=$1
+  echo "-------------------------"
+  echo "[ $msg ]"
+  echo "-------------------------"
+}
+
 # Init and configure Helm
-echo "Adding Jenkins repo and get latest updates"
+log "Adding Jenkins repo and get latest updates"
 helm repo add jenkinsci https://charts.jenkins.io
 helm repo update
 
 # Create Persistent Volume
-echo "Create the volume"
+log "Create the volume"
 kubectl apply -f jenkins-volume.yaml
-echo "Check the volume"
+log "Check the volume"
 kubectl get pv
 
 # Create a service account
@@ -17,30 +24,31 @@ kubectl apply -f jenkins-sa.yaml
 # Update the values manually: Done. TODO: Automate this step
 
 # Deploy
-echo "Start deploying..."
+log "Start deploying..."
 chart="jenkinsci/jenkins"
 # helm install jenkins -n jenkins -f jenkins-values.yaml $chart
 helm upgrade --install jenkins -n jenkins -f jenkins-values.yaml $chart
 
 # Check deployment
-echo "Check deployment"
+log "Check deployment"
 kubectl get pods -n jenkins
 
 # Waiting for port up and running
-while [[ $(kubectl get pods -n jenkins jenkins-0 -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" && sleep 1; done
+pod_name_from_helm="jenkins-0"
+while [[ $(kubectl get pods -n jenkins $pod_name_from_helm -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod - $pod_name_from_helm" && sleep 1; done
 
 # Get metadata
-echo "Get 'admin' password"
+log "Get 'admin' password"
 jsonpath="{.data.jenkins-admin-password}"
 secret=$(kubectl get secret -n jenkins jenkins -o jsonpath=$jsonpath)
-echo $(echo $secret | base64 --decode)
+log $(log $secret | base64 --decode)
 
 # Portforward
-echo "Port forwarding..."
+log "Port forwarding..."
 kubectl port-forward service/jenkins 8090:8080 -n jenkins &
-echo "Waiting 15s for port forward process completed..."
+log "Waiting 15s for port forward process completed..."
 sleep 15
 # login URL
 login_url="http://localhost:8090/login"
 curl $login_url
-echo $login_url
+log $login_url
