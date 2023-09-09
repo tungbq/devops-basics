2e35c9b49447e3f7f48b58ff4c75260934b62e5f
Tung Bui <tung.bquang@gmail.com>
2023-09-09 15:52:53 +0700

    groovy: improvement for more readable

--- topics/groovy/helloworld/basic-concept.groovy
+++ topics/groovy/helloworld/basic-concept.groovy
@@ -1,12 +1,18 @@
 // Greetings!
-println "Welcome to groovy!"
+println "Welcome to Groovy!"
 
+//------------------------
 // Working with variables
-myName = "Tung"
-myAge = 28
+//------------------------
+
+def myName = "Tung"
+def myAge = 28
 println "${myName} is ${myAge} years old!"
 
-// Working with if else
+//-------------------
+// Working with if-else
+//-------------------
+
 def number = 10
 
 if (number > 5) {
@@ -15,7 +21,10 @@ if (number > 5) {
     println("The number is not greater than 5.")
 }
 
-// Working with switch case
+//-----------------------
+// Working with switch-case
+//-----------------------
+
 def myFruit = "Banana"
 
 switch (myFruit) {
@@ -32,14 +41,20 @@ switch (myFruit) {
         println("It's something else.")
 }
 
+//--------------------
 // Working with for loop
+//--------------------
+
 def fruits = ["Apple", "Banana", "Cherry", "Watermelon", "Elderberry"]
 
 for (fruit in fruits) {
     println("I like $fruit")
 }
 
-// Working with function
+//----------------------
+// Working with functions
+//----------------------
+
 def factorial(n) {
     if (n == 0) {
         return 1
@@ -47,12 +62,17 @@ def factorial(n) {
         return n * factorial(n - 1)
     }
 }
-//-- Call the function and store the result in a variable
+
+// Call the function and store the result in a variable
 def result = factorial(5)
-//-- Print the result
+
+// Print the result
 println("Factorial of 5 is: $result")
 
-// Working with Map in groovy
+//-----------------------
+// Working with Map in Groovy
+//-----------------------
+
 // Create a map with key-value pairs
 def person = [
     "firstName": "John",
@@ -60,19 +80,26 @@ def person = [
     "age": 30,
     "city": "New York"
 ]
+
 // Access values in the map
 def firstName = person["firstName"]
 def age = person["age"]
+
 // Modify values in the map
 person["city"] = "Los Angeles"
+
 // Add a new key-value pair to the map
 person["email"] = "john.doe@example.com"
+
 // Iterate over the map
 person.each { key, value ->
     println("$key: $value")
 }
 
-// Working with try catch
+//--------------------
+// Working with try-catch
+//--------------------
+
 def divideNumbers(int dividend, int divisor) {
     try {
         def result = dividend / divisor
@@ -86,8 +113,8 @@ def divideNumbers(int dividend, int divisor) {
 
 // Example usage:
 try {
-    def devideResult = divideNumbers(10, 2)
-    println("Result: $devideResult")
+    def divideResult = divideNumbers(10, 2)
+    println("Result: $divideResult")
 } catch (Exception e) {
     println("An exception occurred: ${e.message}")
 }
