89d2ae2236888c2cc74a2aa72a26405f5e5756be
Tung Bui <tung.bquang@gmail.com>
2023-09-09 15:40:50 +0700

    groovy: add more example

--- topics/groovy/helloworld/basic-concept.groovy
+++ topics/groovy/helloworld/basic-concept.groovy
@@ -14,3 +14,41 @@ if (number > 5) {
 } else {
     println("The number is not greater than 5.")
 }
+
+// Working with switch case
+def myFruit = "Banana"
+
+switch (myFruit) {
+    case "Apple":
+        println("It's an apple.")
+        break
+    case "Banana":
+        println("It's a banana.")
+        break
+    case "Cherry":
+        println("It's a cherry.")
+        break
+    default:
+        println("It's something else.")
+}
+
+// Working with for loop
+def fruits = ["Apple", "Banana", "Cherry", "Watermelon", "Elderberry"]
+
+for (fruit in fruits) {
+    println("I like $fruit")
+}
+
+// Working with function
+def factorial(n) {
+    if (n == 0) {
+        return 1
+    } else {
+        return n * factorial(n - 1)
+    }
+}
+//-- Call the function and store the result in a variable
+def result = factorial(5)
+//-- Print the result
+println("Factorial of 5 is: $result")
+
