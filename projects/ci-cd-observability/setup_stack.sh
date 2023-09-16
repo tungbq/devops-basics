a52c62e1d2f4e491eed39ae5b811f43502082b1f
Tung Bui <tung.bquang@gmail.com>
2023-09-16 15:55:18 +0700

    project/ci-cd-observability: note

--- projects/ci-cd-observability/setup_stack.sh
+++ projects/ci-cd-observability/setup_stack.sh
@@ -7,7 +7,7 @@ cd "$CUR_DIR/../../topics/helm/hands-on/deploy-jenkins/"
 ls -la
 ./deploy.sh
 
-echo "Start ELK"
-cd "$CUR_DIR/../../topics/elk/helloworld/installation/docker-compose"
-ls -la
-./install.sh
+# echo "Start ELK"
+# cd "$CUR_DIR/../../topics/elk/helloworld/installation/docker-compose"
+# ls -la
+# ./install.sh
