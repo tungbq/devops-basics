a42b3db0471f442cee4a36bf3b0cc7ec98c2deae
Tung Bui <tung.bquang@gmail.com>
2023-09-22 22:27:39 +0700

    using docker compose

--- topics/elk/elasalert/deploy.sh
+++ topics/elk/elasalert/deploy.sh
@@ -2,10 +2,5 @@
 mkdir -p workspace
 cd workspace
 git clone https://github.com/bitsensor/elastalert.git; cd elastalert
-docker run -d -p 3030:3030 \
-    -v `pwd`/config/elastalert.yaml:/opt/elastalert/config.yaml \
-    -v `pwd`/config/config.json:/opt/elastalert-server/config/config.json \
-    -v `pwd`/rules:/opt/elastalert/rules \
-    -v `pwd`/rule_templates:/opt/elastalert/rule_templates \
-    --net="host" \
-    --name elastalert bitsensor/elastalert:latest
+
+docker-compose up -d
