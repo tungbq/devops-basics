f28e434dde043e8772dd887a3e55f9416e2829ef
Tung Bui <tung.bquang@gmail.com>
2023-09-26 08:04:50 +0700

    elk: add offline test

--- topics/elk/elasalert/tests/test_rules.sh
+++ topics/elk/elasalert/tests/test_rules.sh
@@ -1,3 +1,8 @@
 
+# document: https://elastalert2.readthedocs.io/en/latest/ruletypes.html#testing-your-rule
 echo "Running rules check..."
 docker exec -it elastalert elastalert-test-rule "/opt/elastalert/rules/metricbeat_cpu_rule.yaml"
+
+echo "Running rules check with local data..."
+docker cp data/json_debug.json elastalert:/tmp/json_debug.json
+docker exec -it elastalert elastalert-test-rule --data /tmp/json_debug.json "/opt/elastalert/rules/metricbeat_cpu_rule.yaml"
