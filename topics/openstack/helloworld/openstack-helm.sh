9acac639952ae239ab278dd8aa6442ee6aa3ccf4
Tung Bui <tung.bquang@gmail.com>
2023-12-25 23:40:44 +0700

    fix sed cmd issue

--- topics/openstack/helloworld/openstack-helm.sh
+++ topics/openstack/helloworld/openstack-helm.sh
@@ -12,9 +12,10 @@ console_log() {
 }
 
 format_and_execute() {
-  local script_path="$1"
-  sed -i -e "$script_path"
-  "./$script_path"
+  echo "Working on $1"
+  script_path=$1
+  sed -i -e 's/\r$//' $script_path
+  "${script_path}"
 }
 
 DEPLOYMENT_DIR="/tmp/osh"
@@ -40,57 +41,57 @@ console_log "Prepare Kubernetes"
 cd "$DEPLOYMENT_DIR/openstack-helm"
 ls -la
 pwd
-format_and_execute "tools/deployment/common/prepare-k8s.sh"
+format_and_execute ./tools/deployment/common/prepare-k8s.sh
 
 # Deploy Ceph
 console_log "Deploy Ceph"
 cd "$DEPLOYMENT_DIR/openstack-helm-infra"
 ls -la
-format_and_execute tools/deployment/ceph/ceph-rook.sh
-format_and_execute tools/deployment/ceph/ceph-adapter-rook.sh
+format_and_execute ./tools/deployment/ceph/ceph-rook.sh
+format_and_execute ./tools/deployment/ceph/ceph-adapter-rook.sh
 
 #Setup OpenStack client
 console_log "Setup OpenStack client"
 cd "$DEPLOYMENT_DIR/openstack-helm"
-format_and_execute tools/deployment/common/setup-client.sh
+format_and_execute ./tools/deployment/common/setup-client.sh
 
 # Traffic Routing to Ceph Rados Gateway Service
 console_log "Traffic Routing to Ceph Rados Gateway Service"
 cd "$DEPLOYMENT_DIR/openstack-helm"
-format_and_execute tools/deployment/component/common/ingress.sh
+format_and_execute ./tools/deployment/component/common/ingress.sh
 
 # Deploy OpenStack backend
 console_log "Deploy OpenStack backend"
 cd "$DEPLOYMENT_DIR/openstack-helm"
-format_and_execute tools/deployment/component/common/rabbitmq.sh
-format_and_execute tools/deployment/component/common/mariadb.sh
-format_and_execute tools/deployment/component/common/memcached.sh
+format_and_execute ./tools/deployment/component/common/rabbitmq.sh
+format_and_execute ./tools/deployment/component/common/mariadb.sh
+format_and_execute ./tools/deployment/component/common/memcached.sh
 
 # Deploy OpenStack
 # Keystone
 console_log "Keystone"
 cd "$DEPLOYMENT_DIR/openstack-helm"
-format_and_execute tools/deployment/component/keystone/keystone.sh
+format_and_execute ./tools/deployment/component/keystone/keystone.sh
 
 # Heat
 console_log "Heat"
 cd "$DEPLOYMENT_DIR/openstack-helm"
-format_and_execute tools/deployment/component/heat/heat.sh
+format_and_execute ./tools/deployment/component/heat/heat.sh
 
 # Glance
 cd "$DEPLOYMENT_DIR/openstack-helm"
-format_and_execute tools/deployment/component/glance/glance.sh
+format_and_execute ./tools/deployment/component/glance/glance.sh
 
 # Placement, Nova, Neutron
 console_log "Placement, Nova, Neutron"
 cd "$DEPLOYMENT_DIR/openstack-helm"
-format_and_execute tools/deployment/component/compute-kit/openvswitch.sh
-format_and_execute tools/deployment/component/compute-kit/libvirt.sh
-format_and_execute tools/deployment/component/compute-kit/compute-kit.sh
+format_and_execute ./tools/deployment/component/compute-kit/openvswitch.sh
+format_and_execute ./tools/deployment/component/compute-kit/libvirt.sh
+format_and_execute ./tools/deployment/component/compute-kit/compute-kit.sh
 
 # Cinder
 console_log "Cinder"
 cd "$DEPLOYMENT_DIR/openstack-helm"
-format_and_execute tools/deployment/component/cinder/cinder.sh
+format_and_execute ./tools/deployment/component/cinder/cinder.sh
 
 console_log "Congrats!"
